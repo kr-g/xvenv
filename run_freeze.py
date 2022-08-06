abfafef04aac9b937f7c2622d35129ecb640d2e9
kr-g <k.r.goger+github@gmail.com>
2022-08-06 14:12:35 +0200

    rework

--- run_freeze.py
+++ run_freeze.py
@@ -1,4 +1,5 @@
 from setuputil import bump_requirements
 
+
 if __name__ == "__main__":
     bump_requirements()
--- setuputil.py
+++ setuputil.py
@@ -14,7 +14,6 @@ import json
 import subprocess
 
 import setuptools
-import distutils
 
 #
 
