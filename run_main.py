50687f9e94752009aa85a181d3f02a7adc46a969
kr-g <k.r.goger+github@gmail.com>
2022-08-07 06:30:20 +0200

    setup change

--- run_main.py
+++ run_main.py
@@ -1,7 +1,9 @@
 import os
 from setuputil import do_main_import
 
-os.chdir(os.path.dirname(__file__))
+cwd = os.path.dirname(__file__)
+cwd = "." if len(cwd) == 0 else cwd
+os.chdir(cwd)
 
 mod = do_main_import()
 mod.main_func()
